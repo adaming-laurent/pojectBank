fa94287cccd34596de0a6f406add930bb5a41a6d
Sachet <adaming.laurent@gmail.com>
2020-11-16 14:42:27 +0100

    first commit project ready

---
+++ src/test/java/com/DevOps/GestionCompteBancaire/GestionCompteBancaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevOps.GestionCompteBancaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCompteBancaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
